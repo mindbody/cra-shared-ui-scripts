8517dde267a446d0a302aa187406d3a292c28317
Cameron Zaas <cameron.zaas@mindbodyonline.com>
2020-04-29 08:28:08 -0700

    feat: added support for custom env file

--- scripts/shared.js
+++ scripts/shared.js
@@ -1,11 +1,16 @@
 const fs = require('fs-extra');
+const commandLineArgs = require('command-line-args');
+
+const optionDefinitions = [{ name: 'env' }];
+const options = commandLineArgs(optionDefinitions);
+const { env = '.env' } = options;
 
 function getEnvironmentCdn() {
     return new Promise(async (resolve, reject) => {
         try {
             const userDir = process.cwd();
             // Read the `.env` file in root of repo and return for the value of `PUBLIC_URL`
-            const userEnv = await fs.readFile(`${userDir}/.env`, 'utf8');
+            const userEnv = await fs.readFile(`${userDir}/${env}`, 'utf8');
             const cdn = userEnv
                 .match(/PUBLIC_URL=.*/)[0]
                 .replace(/PUBLIC_URL=/, '')
@@ -14,7 +19,7 @@ function getEnvironmentCdn() {
             resolve(cdn);
         } catch (e) {
             console.error(e);
-            console.error('=== Make sure the `.env` file exists and contains `PUBLIC_URL` path ===');
+            console.error(`=== Make sure the "${env}" file exists and contains "PUBLIC_URL" path ===`);
             process.exit(1);
         }
     });
@@ -27,15 +32,15 @@ function addVersionToEnvFile() {
             const { version } = await fs.readJson(`${userDir}/package.json`);
             console.log('add version', version);
             // Read the `.env` file in root of repo and return for the value of `PUBLIC_URL`
-            const userEnv = await fs.readFile(`${userDir}/.env`, 'utf8');
+            const userEnv = await fs.readFile(`${userDir}/${env}`, 'utf8');
             const cdn = userEnv.match(/PUBLIC_URL=.*/)[0];
 
-            await fs.writeFile(`${userDir}/.env`, userEnv.replace(cdn, `${cdn}${version}/`));
+            await fs.writeFile(`${userDir}/${env}`, userEnv.replace(cdn, `${cdn}${version}/`));
 
             resolve(cdn);
         } catch (e) {
             console.error(e);
-            console.error('=== There was an issue adding the version from the env file ===');
+            console.error(`=== There was an issue adding the version from the "${env}" file ===`);
             process.exit(1);
         }
     });
@@ -46,15 +51,15 @@ async function removeVersionFromEnvFile() {
         try {
             const userDir = process.cwd();
             const { version } = await fs.readJson(`${userDir}/package.json`);
-            const userEnv = await fs.readFile(`${userDir}/.env`, 'utf8');
+            const userEnv = await fs.readFile(`${userDir}/${env}`, 'utf8');
             const cdn = userEnv.match(/PUBLIC_URL=.*/)[0];
             const cdnNoVersion = cdn.replace(`${version}/`, '');
 
-            await fs.writeFile(`${userDir}/.env`, userEnv.replace(cdn, cdnNoVersion));
+            await fs.writeFile(`${userDir}/${env}`, userEnv.replace(cdn, cdnNoVersion));
             console.log('remove version', version);
         } catch (e) {
             console.error(e);
-            console.error('=== There was an issue removing the version from the env file ===');
+            console.error(`=== There was an issue removing the version from the "${env}" file ===`);
             process.exit(1);
         }
     });
