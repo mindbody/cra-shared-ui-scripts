9755ac4057b926dafae9e1beaa223d5265a07c33
Cameron Zaas <cameron.zaas@mindbodyonline.com>
2020-05-12 11:33:30 -0700

    fix: pulling cdn URL from main env file

--- scripts/shared.js
+++ scripts/shared.js
@@ -10,7 +10,7 @@ function getEnvironmentCdn() {
         try {
             const userDir = process.cwd();
             // Read the `.env` file in root of repo and return for the value of `PUBLIC_URL`
-            const userEnv = await fs.readFile(`${userDir}/${env}`, 'utf8');
+            const userEnv = await fs.readFile(`${userDir}/.env`, 'utf8');
             const cdn = userEnv
                 .match(/PUBLIC_URL=.*/)[0]
                 .replace(/PUBLIC_URL=/, '')
